ac867f92c1ef1673e8b13c77586490745eddd5b2
MHSnodgrass <matthewsnodgrass@live.com>
2021-07-24 17:21:10 -0300

    Fixed typo that was causing Fonts to crash the app

--- App.js
+++ App.js
@@ -7,7 +7,7 @@ import * as Font from 'expo-font'
 const fetchFonts = () => {
   return Font.loadAsync({
     'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
-    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.tff')
+    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf')
   })
 }
 
